8c74f4fd92c7c26592b5539e213732aaa5b9af30
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2026-09-15 15:43:54 +0000

    Deploying to gh-pages from @ abhiabhi94/arrow-game@8513c70ea5c894778816a991c8a7e93257c2b872 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3261063714" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "289580135" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
